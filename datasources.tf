b864d3c8a02b4077c75c6ff41912112b9d651b57
Duval Tagne <duvaltagne@gmail.com>
2026-02-04 14:13:57 +0100

    change ec2 aassume role

--- datasources.tf
+++ datasources.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "ecs_assume_role_policy" {
     actions = [ "sts:AssumeRole" ]
     principals {
       type = "Service"
-      identifiers = [ "ecs.amazonaws.com" ]
+      identifiers = [ "ec2.amazonaws.com" ]
     }
   }
 }
