23134889890ad6a701abeaeb2a64496d8d9ad4e8
Duval Tagne <duvaltagne@gmail.com>
2026-02-04 14:54:26 +0100

    rere apply

--- datasources.tf
+++ datasources.tf
@@ -17,12 +17,12 @@ data "aws_iam_policy_document" "ec2_assume_role_policy" {
     }
   }
 }
-data "aws_iam_policy_document" "ecs_assume_role_policy" {
-  statement {
-    actions = [ "sts:AssumeRole" ]
-    principals {
-      type = "Service"
-      identifiers = [ "ecs.amazonaws.com" ]
-    }
-  }
-}
+# data "aws_iam_policy_document" "ecs_assume_role_policy" {
+#   statement {
+#     actions = [ "sts:AssumeRole" ]
+#     principals {
+#       type = "Service"
+#       identifiers = [ "ecs.amazonaws.com" ]
+#     }
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-# module "lb"{
-#     source = "./modules/lb"
-# }
+module "lb"{
+    source = "./modules/lb"
+}
 
-# module "ecs" {
-#   source = "./modules/ecs"
-#   security_group = ["${module.lb.sg_id}"]
-#   lb_target_group_arn = module.lb.lb-tg-arn
-#   ec2_instance_profile_role_name = module.iam-role-instance.role_name
-#   infrastructure_role_arn = module.iam-role-infra.role_arn
-#   managed_instances = var.managed_instances
-#   requires_compatibilities = var.requires_compatibilities
-# }
+module "ecs" {
+  source = "./modules/ecs"
+  security_group = ["${module.lb.sg_id}"]
+  lb_target_group_arn = module.lb.lb-tg-arn
+  ec2_instance_profile_role_name = module.iam-role-instance.role_name
+  infrastructure_role_arn = module.iam-role-infra.role_arn
+  managed_instances = var.managed_instances
+  requires_compatibilities = var.requires_compatibilities
+}
 
-# module "iam-role-instance" {
-#   source="./modules/iamRole"
-#   role_name = "ecsInstanceRoleManagedInstance"
-#   policies_to_attach = [data.aws_iam_policy.instance-role-policy.arn]
-#   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
-# }
+module "iam-role-instance" {
+  source="./modules/iamRole"
+  role_name = "ecsInstanceRoleManagedInstance"
+  policies_to_attach = [data.aws_iam_policy.instance-role-policy.arn]
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
 
-# module "iam-role-infra" {
-#   source="./modules/iamRole"
-#   role_name = "ecsManagedInstanceInfrastructureRole"
-#   policies_to_attach = [data.aws_iam_policy.infra-role-policy.arn,data.aws_iam_policy.infra-role-volume-policy.arn]
-#   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
-# }
+module "iam-role-infra" {
+  source="./modules/iamRole"
+  role_name = "ecsManagedInstanceInfrastructureRole"
+  policies_to_attach = [data.aws_iam_policy.infra-role-policy.arn,data.aws_iam_policy.infra-role-volume-policy.arn]
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
