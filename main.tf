a4739d0552ea64c8bdecad5c2e1fc05ab332b952
Duval Tagne <duvaltagne@gmail.com>
2026-02-04 10:07:23 +0100

    fix error assume role json data policy

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ module "ecs" {
 module "iam-role-instance" {
   source="./modules/iamRole"
   role_name = "ecsInstanceRoleManagedInstance"
-  policies_to_attach = [data.aws_iam_policy.instance-role-policy.json]
+  policies_to_attach = [data.aws_iam_policy.instance-role-policy.arn]
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
 }
 
 module "iam-role-infra" {
   source="./modules/iamRole"
   role_name = "ecsManagedInstanceInfrastructureRole"
-  policies_to_attach = [data.aws_iam_policy.infra-role-policy.json,data.aws_iam_policy.infra-role-volume-policy.json]
+  policies_to_attach = [data.aws_iam_policy.infra-role-policy.arn,data.aws_iam_policy.infra-role-volume-policy.arn]
   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role_policy.json
 }
