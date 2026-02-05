e26d618c82fb3ff0c1797ce3471bc24348bf4c13
Duval Tagne <duvaltagne@gmail.com>
2026-02-05 11:47:12 +0100

    rerere apply with ecs capacity provider

--- modules/ecs/ecs_capacity_provider.tf
+++ modules/ecs/ecs_capacity_provider.tf
@@ -1,7 +1,6 @@
 resource "aws_ecs_capacity_provider" "this" {
   count=var.managed_instances==0?0:1
   name    = "${var.app-name}-managed-instances-cp"
-  cluster = aws_ecs_cluster.cluster.name
 
   managed_instances_provider {
     infrastructure_role_arn = var.infrastructure_role_arn
@@ -25,4 +24,19 @@ resource "aws_iam_instance_profile" "ecs_instance_managed" {
   count=var.managed_instances==0?0:1
   name="${var.ec2_instance_profile_role_name}Profile"
   role=var.ec2_instance_profile_role_name
+}
+
+
+
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  count=var.managed_instances==0?0:1
+  cluster_name = aws_ecs_cluster.cluster.name
+
+  capacity_providers = var.requires_compatibilities != "FARGATE" ? [aws_ecs_capacity_provider.this[0].name] : ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 2
+    weight            = 1
+    capacity_provider = var.requires_compatibilities != "FARGATE" ? aws_ecs_capacity_provider.this[0].name : "FARGATE"
+  }
 }
